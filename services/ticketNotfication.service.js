8dfc84acf223fcf3a0b4609c0314477a431aa529
sanjeetboora <sanjeet.boora97@gmail.com>
2023-04-21 23:29:35 +0530

    send status id fixed

--- services/ticketNotfication.service.js
+++ services/ticketNotfication.service.js
@@ -1,13 +1,13 @@
 const TicketNotification = require('../models/ticketNotification.model');
 
-const createTicketNotification = async (data) =>{
-    try{
+const createTicketNotification = async (data) => {
+    try {
         const ticketNotificationObj = {
             subject: data.subject,
             content: data.content,
             recepientEmails: data.recepientEmails,
             requester: data.requester,
-            ticketId:data.ticketId,
+            ticketId: data.ticketId,
         }
 
         const notification = await TicketNotification.create(ticketNotificationObj);
@@ -15,36 +15,36 @@ const createTicketNotification = async (data) =>{
             notification: notification
         }
     }
-    catch(err){
+    catch (err) {
         console.log(err.message);
         return err.message;
     }
 }
 
-const getAllUnsentNotifications = async() =>{
-    try{
+const getAllUnsentNotifications = async () => {
+    try {
         const unsentNotifications = await TicketNotification.find({
             sentStatus: "UNSENT"
         });
         return unsentNotifications;
     }
-    catch(err){
+    catch (err) {
         console.log(err.message);
         return err.message;
     }
 }
 
-const setStatusSent = async(notification) =>{
-    try{
+const setStatusSent = async (notification) => {
+    try {
         const update = notification;
         update.sentStatus = "SENT";
-        await TicketNotification.updateOne({id: notification._id}, update);
+        await TicketNotification.updateOne({ _id: notification._id }, update);
     }
-    catch(err){
+    catch (err) {
         console.log(err.message);
         return err.message;
     }
 }
 
 
-module.exports = {createTicketNotification, getAllUnsentNotifications, setStatusSent};
+module.exports = { createTicketNotification, getAllUnsentNotifications, setStatusSent };
